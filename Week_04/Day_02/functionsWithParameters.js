511133459d4edbd91c342518ba8243d8d3a70e2b
matt-codeslo <matt@codeslo.com>
2019-03-05 16:22:00 -0800

    added code example

---
+++ Week_04/Day_02/functionsWithParameters.js
@@ -0,0 +1,8 @@
+function giveMeParameters(param1,param2){
+    console.log("Parameter one is: ",param1);
+    console.log("Parameter two is: ",param2);
+    return "Those are the parameters I received from the function call.";
+}
+
+// calling the function 
+giveMeParameters('foo','bar');
