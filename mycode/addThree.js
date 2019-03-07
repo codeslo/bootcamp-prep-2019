ce723b8889f2fd289922e8fb4cfe5f8c11f09087
matt-codeslo <matt@codeslo.com>
2019-03-07 10:47:49 -0800

    deleting untracked

--- mycode/addThree.js
+++
@@ -1,5 +0,0 @@
-function addThree(a,b,c){
-    return a + b + c;
-}
-
-console.log(addThree(1,2,"pizza"));
--- mycode/firstFunction.js
+++
@@ -1,8 +0,0 @@
-function tenTimesTen(){
-    return 10 * 10;
-}
-
-let result = tenTimesTen();
-
-console.log(result);
-console.log(tenTimesTen());
