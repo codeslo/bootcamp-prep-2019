27d7cecdc8c0bd84ac69aec6ca61e9f6ba168603
matt-codeslo <matt@codeslo.com>
2019-05-16 17:21:50 -0700

    removed duplicate files

--- Week_03/Day_01/first-function-complete.js
+++
@@ -1,9 +0,0 @@
-//Define the function
-function timesTen() {
-    console.log(10 * 10);
-}
-
-//Call the function
-timesTen();
-
-
--- Week_03/Day_01/hello-world-2-complete.js
+++
@@ -1,6 +0,0 @@
-let firstString = 'Hello';
-let secondString = 'World';
-
-let sentence = firstString + ' ' + secondString + '!';
-
-console.log(sentence);
