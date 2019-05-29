b12c26c530b5bf777cfb25e4463200e32a548b56
matt-codeslo <matt@codeslo.com>
2019-05-28 18:44:05 -0700

    fixed gotApples

--- Week_03/Day_04/gotApples.js
+++ Week_03/Day_04/gotApples.js
@@ -1,16 +1,6 @@
-// what is the index position of 'apples' in this array?
+// what is the index position of 'apples' in arr?
+// does arr contain 'guava'?
 
-let arr = ['banana','pear','tangerine','apple','grape','peach'];
+let arr = ["banana", "pear", "tangerine", "apple", "grape", "peach"];
 
-function findFruit(fruit){
-    if(arr.indexOf(fruit) !== -1){
-        return "I found your "+fruit+" at index position "+arr.indexOf(fruit);
-    }else{
-        return "I don't have any "+fruit;
-    }
-}
-
-console.log(findFruit('apple'));
-console.log(findFruit('guava'));
-
-// does arr contain 'guava'? 
+function findFruit(fruit) {}
